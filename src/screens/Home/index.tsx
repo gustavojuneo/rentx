812e8c55efb6a64bdf1fce9b762f8748ad461999
Gustavo Santos <gustavojuneo@outlook.com>
2021-06-15 22:06:17 -0300

    Create Splash Screen with Animation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useTheme } from 'styled-components';
-import { useNavigation } from '@react-navigation/core';
-import { StatusBar } from 'expo-status-bar';
-import { Ionicons } from '@expo/vector-icons';
-import { RFValue } from 'react-native-responsive-fontsize';
+import React, { useEffect, useState } from 'react'
+import { useTheme } from 'styled-components'
+import { useNavigation } from '@react-navigation/core'
+import { StatusBar } from 'expo-status-bar'
+import { Ionicons } from '@expo/vector-icons'
+import { RFValue } from 'react-native-responsive-fontsize'
 
-import Logo from '../../assets/logo.svg';
+import Logo from '../../assets/logo.svg'
 
-import { api } from '../../services/api';
-import { CarDTO } from '../../dtos/CarDTO';
-import { Car } from '../../components/Car';
-import { Loading } from '../../components/Loading';
+import { api } from '../../services/api'
+import { CarDTO } from '../../dtos/CarDTO'
+import { Car } from '../../components/Car'
+import { Loading } from '../../components/Loading'
 
 import {
   Container,
@@ -18,37 +18,37 @@ import {
   HeaderContent,
   TotalCars,
   CarList,
-  MyCarsButton,
-} from './styles';
+  MyCarsButton
+} from './styles'
 
 export function Home() {
-  const navigation = useNavigation();
-  const theme = useTheme();
-  const [loading, setLoading] = useState(true);
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const navigation = useNavigation()
+  const theme = useTheme()
+  const [loading, setLoading] = useState(true)
+  const [cars, setCars] = useState<CarDTO[]>([])
 
   function handleCarDetails(car: CarDTO) {
-    navigation.navigate('CarDetails', { car });
+    navigation.navigate('CarDetails', { car })
   }
 
   function handleOpenMyCars() {
-    navigation.navigate('MyCars');
+    navigation.navigate('MyCars')
   }
 
   async function getCars() {
     try {
-      const { data } = await api.get('cars');
-      setCars(data);
+      const { data } = await api.get('cars')
+      setCars(data)
     } catch (err) {
-      console.log(err);
+      console.log(err)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   }
 
   useEffect(() => {
-    getCars();
-  }, []);
+    getCars()
+  }, [])
 
   return (
     <Container>
@@ -57,7 +57,7 @@ export function Home() {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>Total de {cars.length} carros</TotalCars>
         </HeaderContent>
       </Header>
 
@@ -80,5 +80,5 @@ export function Home() {
         />
       </MyCarsButton>
     </Container>
-  );
+  )
 }
